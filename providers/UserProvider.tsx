139ffd668e045c944981d8fa0dd4f00bcbfd3768
George <george1nikiforos@gmail.com>
2023-08-27 17:35:13 +0300

    added supabase setup and providers for auth

---
+++ providers/UserProvider.tsx
@@ -0,0 +1,17 @@
+"use client";
+
+import MyUserContextProvider from "@/hooks/useUser";
+import { ReactNode } from "react";
+
+interface UserProviderProps {
+    children: ReactNode;
+};
+
+
+export default function UserProvider({children}: UserProviderProps) {
+  return (
+    <MyUserContextProvider>
+        {children}
+    </MyUserContextProvider>
+  )
+}
